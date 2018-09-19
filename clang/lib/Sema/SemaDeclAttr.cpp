cms-externals/clang

Changes to clang to implement cms custom CXX11 attributes

lib/Sema/SemaDeclAttr.cpp
@@ -5941,6 +5941,37 @@ static void handleOpenCLAccessAttr(Sema &S, Decl *D,
       Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
 
+static void handleCMSThreadSafeAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+
+    assert(!Attr.isInvalid());
+
+    if (!(isa<Decl>(D))) {
+      S.Diag(Attr.getLoc(), diag::warn_attribute_wrong_decl_type)
+        << Attr.getName();
+      return;
+    }
+
+    D->addAttr( ::new (S.Context) CMSThreadSafeAttr(Attr.getRange(), S.Context,
+						Attr.getAttributeSpellingListIndex()));
+}
+
+static void handleCMSThreadGuardAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+    assert(!Attr.isInvalid());
+
+    if (!(isa<Decl>(D) ))  {
+      S.Diag(Attr.getLoc(), diag::warn_attribute_wrong_decl_type)
+        << Attr.getName() << ExpectedVariableOrFunction;
+      return;
+    }
+    StringRef Str;
+    if (!S.checkStringLiteralArgumentAttr(Attr, 0, Str))
+      return;
+
+  D->addAttr(::new (S.Context) CMSThreadGuardAttr(Attr.getRange(), S.Context, Str,
+                                         Attr.getAttributeSpellingListIndex()));
+
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -6576,6 +6607,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case AttributeList::AT_XRayLogArgs:
     handleXRayLogArgsAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_CMSThreadSafe:
+    handleCMSThreadSafeAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_CMSThreadGuard:
+    handleCMSThreadGuardAttr(S, D, Attr);
     break;
   }
 }

cms-externals/llvm-project-20170507

clang/lib/Sema/SemaDeclAttr.cpp
@@ -5939,6 +5939,36 @@ static void handleOpenCLAccessAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
       AL.getRange(), S.Context, AL.getAttributeSpellingListIndex()));
 }
 
+static void handleCMSThreadSafeAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
+    assert(!AL.isInvalid());
+
+    if (!(isa<Decl>(D))) {
+      S.Diag(AL.getLoc(), diag::warn_attribute_wrong_decl_type)
+        << AL.getName();
+      return;
+    }
+
+    D->addAttr(::new (S.Context) CMSThreadSafeAttr(AL.getRange(), S.Context,
+                                                   AL.getAttributeSpellingListIndex()));
+}
+
+static void handleCMSThreadGuardAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
+    assert(!AL.isInvalid());
+
+    if (!(isa<Decl>(D) ))  {
+      S.Diag(AL.getLoc(), diag::warn_attribute_wrong_decl_type)
+        << AL.getName() << ExpectedVariableOrFunction;
+      return;
+    }
+    StringRef Str;
+    if (!S.checkStringLiteralArgumentAttr(AL, 0, Str))
+      return;
+
+  D->addAttr(::new (S.Context) CMSThreadGuardAttr(AL.getRange(), S.Context, Str,
+                                                  AL.getAttributeSpellingListIndex()));
+
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -6603,6 +6633,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case ParsedAttr::AT_XRayLogArgs:
     handleXRayLogArgsAttr(S, D, AL);
+    break;
+  case ParsedAttr::AT_CMSThreadSafe:
+    handleCMSThreadSafeAttr(S, D, AL);
+    break;
+  case ParsedAttr::AT_CMSThreadGuard:
+    handleCMSThreadGuardAttr(S, D, AL);
     break;
   }
 }